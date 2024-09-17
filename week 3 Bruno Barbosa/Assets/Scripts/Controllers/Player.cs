b65c90409c04954a351dc65724aa1e7e1ea6e05c
dg015 <bruno.barbosa8@yahoo.com>
2024-09-17 14:09:05 -0400

    task 1 complete
    Player movement complete

--- week 3 Bruno Barbosa/Assets/Scripts/Controllers/Player.cs
+++ week 3 Bruno Barbosa/Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Codice.Client.Common.GameUI;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,9 +10,46 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    [SerializeField] private float acelaration;
+    [SerializeField] private float speed;
+
+
+
     void Update()
     {
 
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+
+    private void PlayerMovement()
+    {
+        
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            //transform.Translate((Vector2.up * Time.deltaTime) *speed);
+            Debug.Log("up");
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate((Vector2.down * Time.deltaTime) * speed);
+            Debug.Log("down");
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate((Vector2.right * Time.deltaTime) * speed);
+            Debug.Log("right");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate((Vector2.left * Time.deltaTime) * speed);
+            Debug.Log("left");
+        }
     }
 
 }
