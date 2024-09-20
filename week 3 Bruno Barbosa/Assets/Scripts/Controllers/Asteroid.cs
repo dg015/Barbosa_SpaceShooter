449a0d97364374a124c5250f44c835f510075b15
dg015 <bruno.barbosa8@yahoo.com>
2024-09-20 15:53:11 -0400

    task 4 complete

--- week 3 Bruno Barbosa/Assets/Scripts/Controllers/Asteroid.cs
+++ week 3 Bruno Barbosa/Assets/Scripts/Controllers/Asteroid.cs
@@ -7,23 +7,52 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
+    public bool hasReached;
+    Vector3 TargetLocation;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
 
-    public void OnMouseUpAsButton()
+    public void AsteroidMovement()
     {
-        maxFloatDistance = Random.Range(0, maxFloatDistance);
+      
+        //check for when the code is run for the first time(vector will be 0) or if it has arrived
+        if( TargetLocation == Vector3.zero || hasReached )
+        {
+          //create new random location
+          Debug.Log("Im working"+ TargetLocation);
+          TargetLocation = new Vector2((Random.Range(-maxFloatDistance, maxFloatDistance)), (Random.Range(-maxFloatDistance, maxFloatDistance)));
+          hasReached = false;
+        }
+        //get distance between asteroid and target position
+        float distanceFromTarget = Vector2.Distance(transform.position, TargetLocation);
+
+        //draw line to the location just to make it easier
+        Debug.DrawLine(transform.position, (TargetLocation - transform.position).normalized, Color.blue);
+        //check if its in the arrival radius
+        if (distanceFromTarget > arrivalDistance)
+        {
+            //move the asteroid in the direciton of the target position
+            Vector3 direction = (TargetLocation - transform.position).normalized;
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+            
+            Debug.Log("walking toawrds location");
+        }
+        else
+        {
+            Debug.Log("arrived");
+            hasReached = true;
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        AsteroidMovement();
     }
 }
