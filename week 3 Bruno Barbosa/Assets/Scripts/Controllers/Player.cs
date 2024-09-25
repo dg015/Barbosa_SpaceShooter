8593aaa93b612aa8f33f35242eac2d26449aec3d
dg015 <bruno.barbosa8@yahoo.com>
2024-09-24 23:41:23 -0400

    1 attempt at task 1

--- week 3 Bruno Barbosa/Assets/Scripts/Controllers/Player.cs
+++ week 3 Bruno Barbosa/Assets/Scripts/Controllers/Player.cs
@@ -2,6 +2,7 @@
 using Codice.Client.Common.GameUI;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -19,20 +20,52 @@ public class Player : MonoBehaviour
     //enemy radar
     [SerializeField] private float radius;
     [SerializeField] private int circlePoints;
+    [SerializeField] private List<Vector2> points;
+    [SerializeField] private bool hasCompleted;
+
+    private void Start()
+    {
+        points = new List<Vector2>();
+        
+    }
 
     void Update()
     {
+        if( points.Count ==5)
+            hasCompleted = true;
         PlayerMovement();
+        EnemyRadar(radius, circlePoints);
     }
 
     public void EnemyRadar(float radius, int circlePoints)
     {
+        Vector2 nexPoint;
         for (int i = 0; i < circlePoints; i++)
         {
+            if (i == points.Count - 1)//check if its the last star
+            {
+                nexPoint = points[0];//back to first one in the list
+            }
+            else
+            {
+                nexPoint = points[i + 1];//if not then continue as normal
+            }
+            if (!hasCompleted)
+            {
+                //points[i] = new Vector2((Mathf.Cos(i),Mathf.Sin(i)) * radius);
+                float xPos = Mathf.Cos(i);
+                float yPos = Mathf.Sin(i);
+                //points[i] = new Vector2(xPos, yPos) * radius;
+
+                
+
+                points.Add(new Vector2(xPos, yPos) * radius);
 
 
+            }
+            Debug.DrawLine(points[i], nexPoint, Color.green);
         }
-
+        
 
     }
 
@@ -69,8 +102,6 @@ public class Player : MonoBehaviour
             
            float DecelarationRate = (speed /DecelerationTime) * Time.deltaTime;
             acelaration -= DecelarationRate;
-
-
         }
         
     }
