cc3faf2da5a1764b7421dd642b5b0a10def6084b
dg015 <bruno.barbosa8@yahoo.com>
2024-10-01 14:52:47 -0400

    week 5 class ended

--- week 3 Bruno Barbosa/Assets/Scripts/rotationTest.cs
+++ week 3 Bruno Barbosa/Assets/Scripts/rotationTest.cs
@@ -6,6 +6,8 @@ public class rotationTest : MonoBehaviour
 {
     public float AngularSpeed;
     public float currentRotation;
+    public float inAngle;
+
     [Range(0.0f, 180f)]
     public float TargetAngle;
 
@@ -22,13 +24,28 @@ public class rotationTest : MonoBehaviour
         Debug.DrawLine(transform.position, transform.up);
         //currentRotation = Mathf.Atan2(transform.up.y,transform.up.x) * Mathf.Rad2Deg;
         currentRotation = transform.rotation.eulerAngles.z;
-        if (currentRotation + 90 < TargetAngle)
+        if (TargetAngle - currentRotation < 0)
         {
-            transform.Rotate(0, 0, 1 * AngularSpeed * Time.deltaTime);
+            if (currentRotation + 90 > TargetAngle)
+            {
+                transform.Rotate(0, 0, 1 * -AngularSpeed * Time.deltaTime);
+            }
         }
         else
         {
-            Debug.Log("stopped");
+            if (currentRotation + 90 < TargetAngle)
+            {
+                transform.Rotate(0, 0, AngularSpeed * Time.deltaTime);
+            }
         }
+    }
+    public float StandizeAngle(float angle)
+    {
+        inAngle %= 360;
+        if (inAngle > 180)
+        {
+            inAngle -= 360;
+        }
+        return inAngle;
     }
 }
