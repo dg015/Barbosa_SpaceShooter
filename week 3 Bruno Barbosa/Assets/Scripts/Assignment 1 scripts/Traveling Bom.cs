be58930a8cb69726f69a3b77facfcff250bbfbe4
dg015 <bruno.barbosa8@yahoo.com>
2024-10-12 16:25:51 -0400

    Basic script for traveling bombs created
    Bombs travel in the direction they're facing

--- week 3 Bruno Barbosa/Assets/Scripts/Assignment 1 scripts/Traveling Bom.cs
+++ week 3 Bruno Barbosa/Assets/Scripts/Assignment 1 scripts/Traveling Bom.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class TravelingBom : MonoBehaviour
 {
+    //aceleration
+    [SerializeField] private float speed;
+    [SerializeField] private float acelerationIndex;
+    [SerializeField] private float desacelerationIndex;
+    //desaceleration
+    [SerializeField] private float currentAceleration;
+    [SerializeField] private float currentDesacelerationIndex;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,12 @@ public class TravelingBom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
     }
 }
